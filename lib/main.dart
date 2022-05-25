1884200d6033c20c98a00fc6d4edcf2f49df07a1
Oğuzhan Ermiş <32513546+Ossi57@users.noreply.github.com>
2022-05-25 13:23:06 +0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rounded_progress_bar/flutter_icon_rounded_progress_bar.dart';
+import 'package:flutter_rounded_progress_bar/flutter_rounded_progress_bar.dart';
+import 'package:flutter_rounded_progress_bar/rounded_progress_bar_style.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
